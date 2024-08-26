7fa2b29cd8964ddaf54ba1d03fb46982f7037e4b
coffeesnwob <lindelwekhumalo@outlook.com>
2024-08-26 03:26:53 +0200

    scripts update

--- datescript.ps1
+++ datescript.ps1
@@ -1,2 +1,12 @@
+<# -----
+    write-host command writes to the host, printing a given argument to the terminal. The string "The current Date is:" is used as an argument to Write-Host.
+    ------- #>
 Write-Host "The current Date is:"
+<# -----
+    get-date command retrieves and prints the current date to the terminal.
+    ------- #>
 Get-Date
+
+<# -----
+    This scripts prints the current date to the terminal.
+    ------- #>
--- host-commands.ps1
+++ host-commands.ps1
@@ -1,4 +1,13 @@
+<# ------
+    write-host command writes to the host, printing a given argument to the terminal, and in this case, the string "All commands that act on host:" which is used as an argument to Write-Host
+    ------- #>
 Write-Host "All commands that act on Host:"
+
+<# ------
+    get-command is used to discover more commands available to use. In this case, to output all "Host" commands, we pass an argument using the -noun flag
+    ------ #>
 Get-Command -Noun Host
 
-<# This script will print all the commands that are available for the noun Host #>
+<# -----
+    This script will print all the commands that are available for the Noun Host
+    ------ #>
